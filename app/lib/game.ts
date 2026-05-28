d91d37f6c0083c9777a243cb86c7cb147aaccf90
Gabriele <gabriele@Mac.Home>
2026-05-28 10:35:43 +0200

    fix tipi TypeScript game.ts

--- app/lib/game.ts
+++ app/lib/game.ts
@@ -1,3 +1,11 @@
-export const saveScore = async (score, user, profile) => {
-    return supabase.from("scores").insert(...);
-  };
+import { supabase } from "./supabase";
+
+export const saveScore = async (score: number, user: any, profile: any) => {
+  return supabase.from("scores").insert([
+    {
+      name: `${profile.first_name} ${profile.last_name}`,
+      score,
+      company: profile.company,
+    },
+  ]);
+};
